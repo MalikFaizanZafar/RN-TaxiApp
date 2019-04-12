4216878d3bec058698afc2137756af00c1b9c910
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-04-12 14:47:09 +0500

    get location working for now

--- Screens/Landing/map.js
+++ Screens/Landing/map.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Text, View, Image, StyleSheet } from "react-native";
+import { Text, View, Image, StyleSheet, Dimensions, PermissionsAndroid } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { Button } from "react-native-elements";
 export default class MapScreen extends React.Component {
   static navigationOptions = {
     drawerLabel: "Map",
@@ -14,16 +15,47 @@ export default class MapScreen extends React.Component {
       />
     )
   };
+  constructor(props) {
+    super(props);
 
-  render() {
-    const markers = [
-      {
-        latitude: 33.676446,
-        longitude: 72.998152,
-        title: 'SubQuch',
-        subtitle: 'Inverse Square'
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error:null,
+    }
+  } 
+  async LocationSerivce() {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          'title': 'Location Permission',
+          'message': 'This App needs access to your location ' +
+                     'so we can know where you are.'
+        }
+      )
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use locations ")
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            console.log(position);
+            this.setState({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+              error: null,
+            });
+          },
+          (error) => this.setState({ error: error.message }),
+          { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 },
+        )
+      } else {
+        console.log("Location permission denied")
       }
-    ];
+    } catch (err) {
+      console.warn(err)
+    }
+  }
+  render() {
     return (
       <View style={styles.container}>
         <MapView
@@ -43,12 +75,14 @@ export default class MapScreen extends React.Component {
             description={"Inverse Square"}
          />
         </MapView>
-        <Text>Welcome to SubQuch Maps</Text>
+        <Button title="Near My Location" onPress={() => this.LocationSerivce()}/>
+        {/* <Text> You Location : Lat : {this.state.latitude}, Long : {this.state.longitude} </Text> */}
       </View>
     );
   }
 }
 
+let dimensions = Dimensions.get("window");
 const styles = StyleSheet.create({
   container: {
     position: "absolute",
@@ -65,7 +99,8 @@ const styles = StyleSheet.create({
     top: 0,
     left: 0,
     right: 0,
-    bottom: 0
+    bottom: 0,
+    height: dimensions.height * 0.9
   },
   icon: {
     width: 24,
