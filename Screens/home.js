771dec8d1ae60f8b590f051526c10a384a1a1c04
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-04-19 13:48:39 +0500

    removed some extra console logs

--- Screens/home.js
+++ Screens/home.js
@@ -1,13 +1,31 @@
 import React from "react";
-import { StyleSheet, View, Button, Text, Modal, TouchableHighlight, Image } from "react-native";
-import { connect } from 'react-redux';
-import { GoogleSignin, GoogleSigninButton, statusCodes } from "react-native-google-signin";
-import { LoginButton, AccessToken, LoginManager, GraphRequest, GraphRequestManager } from "react-native-fbsdk";
+import {
+  StyleSheet,
+  View,
+  Button,
+  Text,
+  Modal,
+  TouchableHighlight,
+  Image
+} from "react-native";
+import { connect } from "react-redux";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes
+} from "react-native-google-signin";
+import {
+  LoginButton,
+  AccessToken,
+  LoginManager,
+  GraphRequest,
+  GraphRequestManager
+} from "react-native-fbsdk";
 import { addUser } from "../store/actions";
 import signUser from "../services/signUser";
-import { Avatar  } from 'react-native-elements';
-import RadioForm from 'react-native-simple-radio-button';
-import DatePicker from 'react-native-datepicker'
+import { Avatar } from "react-native-elements";
+import RadioForm from "react-native-simple-radio-button";
+import DatePicker from "react-native-datepicker";
 
 const styles = StyleSheet.create({
   container: {
@@ -34,30 +52,31 @@ class HomeScreen extends React.Component {
   };
   state = {
     modalVisible: false
-  }
+  };
   constructor(props) {
     super(props);
     this.state = {
       userInfo: {},
-      gender: '',
-      birthday: '',
+      gender: "",
+      birthday: "",
       modalVisible: false
     };
   }
   setModalVisible(visible) {
-    this.setState({modalVisible: visible});
+    this.setState({ modalVisible: visible });
   }
-  getChecked = (value) => {
+  getChecked = value => {
     // value = our checked value
     this.setState({
       gender: value
-    })
-    console.log(value)
-  }
-  componentDidMount(){
+    });
+    console.log(value);
+  };
+  componentDidMount() {
     GoogleSignin.configure({
       scopes: ["https://www.googleapis.com/auth/drive.readonly"], // what API you want to access on behalf of the user, default is email and profile
-      webClientId: "895935298518-8apvnm51q2vj6ivbm6pbqj7ma5v7dsq9.apps.googleusercontent.com", // client ID of type WEB for your server (needed to verify user ID and offline access)
+      webClientId:
+        "895935298518-8apvnm51q2vj6ivbm6pbqj7ma5v7dsq9.apps.googleusercontent.com", // client ID of type WEB for your server (needed to verify user ID and offline access)
       offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
       hostedDomain: "", // specifies a hosted domain restriction
       loginHint: "", // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
@@ -72,15 +91,15 @@ class HomeScreen extends React.Component {
       this.setState({ userInfo });
       let googleUser = {
         socialId: userInfo.user.id,
-        password: '1234',
+        password: "1234",
         token: userInfo.accessToken,
-        name : userInfo.user.name,
+        name: userInfo.user.name,
         email: userInfo.user.email,
         photo: userInfo.user.photo,
         provider: "GOOGLE"
-      }
-      this.setState({userInfo : googleUser })
-      this.setState({modalVisible: true});
+      };
+      this.setState({ userInfo: googleUser });
+      this.setState({ modalVisible: true });
     } catch (error) {
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
         // user cancelled the login flow
@@ -103,65 +122,64 @@ class HomeScreen extends React.Component {
               source={require("./../assets/subquch.png")}
             />
           </View>
-          <View style={{ marginTop: 20, justifyContent: "center", alignItems: "center" }}>
+          <View
+            style={{
+              marginTop: 20,
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
             <LoginButton
               style={{ width: 230, height: 30 }}
-              onLoginFinished={
-                (error, result) => {
-                  if (error) {
-                    alert("login has error: " + result.error);
-                  } else if (result.isCancelled) {
-                    alert("login is cancelled.");
-                  } else {
-          
-                    AccessToken.getCurrentAccessToken().then(
-                      (data) => {
-                        let accessToken = data.accessToken
-          
-                        const responseInfoCallback = (error, result) => {
-                          if (error) {
-                            console.log(error)
-                            alert('Error fetching data: ' + error.toString());
-                          } else {
-                            console.log(result)
-                            alert('Success fetching data: ' + result.toString());
+              onLoginFinished={(error, result) => {
+                if (error) {
+                  alert("login has error: " + result.error);
+                } else if (result.isCancelled) {
+                  alert("login is cancelled.");
+                } else {
+                  AccessToken.getCurrentAccessToken().then(data => {
+                    let accessToken = data.accessToken;
+
+                    const responseInfoCallback = (error, result) => {
+                      if (error) {
+                        console.log(error);
+                        alert("Error fetching data: " + error.toString());
+                      } else {
+                        console.log(result);
+                        alert("Success fetching data: " + result.toString());
+                      }
+                    };
+
+                    const infoRequest = new GraphRequest(
+                      "/me",
+                      {
+                        accessToken: accessToken,
+                        parameters: {
+                          fields: {
+                            string: "email,name,picture"
                           }
                         }
-          
-                        const infoRequest = new GraphRequest(
-                          '/me',
-                          {
-                            accessToken: accessToken,
-                            parameters: {
-                              fields: {
-                                string: 'email,name,picture'
-                              }
-                            }
-                          },
-                          (err, rslt) => {
-                            // console.log('graphRequest result is : ', rslt)
-                            let facebookUser = {
-                              socialId: rslt.id,
-                              password: '1234',
-                              token: accessToken,
-                              name : rslt.name,
-                              email: rslt.email,
-                              photo: rslt.picture.data.url,
-                              provider: "FACEBOOK"
-                            }
-                            this.setState({userInfo : facebookUser })
-                            this.setState({modalVisible: true})
-                          }
-                        );
-                        // Start the graph request.
-                        new GraphRequestManager().addRequest(infoRequest).start()
-          
+                      },
+                      (err, rslt) => {
+                        // console.log('graphRequest result is : ', rslt)
+                        let facebookUser = {
+                          socialId: rslt.id,
+                          password: "1234",
+                          token: accessToken,
+                          name: rslt.name,
+                          email: rslt.email,
+                          photo: rslt.picture.data.url,
+                          provider: "FACEBOOK"
+                        };
+                        this.setState({ userInfo: facebookUser });
+                        this.setState({ modalVisible: true });
                       }
-                    )
-          
-                  }
+                    );
+                    // Start the graph request.
+                    new GraphRequestManager().addRequest(infoRequest).start();
+                  });
                 }
-              }
+              }}
               onLogoutFinished={() => alert("logout.")}
             />
             <GoogleSigninButton
@@ -172,92 +190,123 @@ class HomeScreen extends React.Component {
             />
           </View>
           <View style={{ marginTop: 8 }}>
-          <Modal
-          style={{justifyContent: "center", alignItems: "center"}}
-          animationType="slide"
-          transparent={false}
-          visible={this.state.modalVisible}
-          onRequestClose={() => {
-            Alert.alert('Modal has been closed.');
-          }}>
-          <View style={{marginTop: 22}}>
-            <View>
-              <View style={{justifyContent: "center", alignItems: "center"}}>
-              <Avatar
-                  rounded
-                  size="xlarge"
-                  title={this.state.userInfo.name}
-                  source={{
-                    uri: this.state.userInfo.photo,
-                  }}
-                />
-                <Text style={{marginTop: 10}}>{this.state.userInfo.name}</Text>
-                <Text style={{marginTop: 5}}>{this.state.userInfo.email}</Text>
-              </View>
-              <View style={{justifyContent: "center", alignItems: "center"}}>
-              </View>
-              <View style={{justifyContent: "center", alignItems: "center"}}>
-              <DatePicker
-                  style={{width: 300, marginTop: 20}}
-                  date={this.state.birthday}
-                  mode="date"
-                  placeholder="Your Date of Birth"
-                  format="DD-MM-YYYY"
-                  minDate="01-01-1950"
-                  maxDate="01-01-2030"
-                  confirmBtnText="Confirm"
-                  cancelBtnText="Cancel"
-                  customStyles={{
-                    dateIcon: {
-                      position: 'absolute',
-                      left: 0,
-                      top: 4,
-                      marginLeft: 0
-                    },
-                    dateInput: {
-                      marginLeft: 36
-                    }
-                  }}
-                  onDateChange={(date) => {this.setState({ birthday: date })
-                }}
-                />
-              </View>
-              <View style={{justifyContent: "center", alignItems: "center", marginTop: 20}}>
-              <Text >Your Gender</Text>
-                <RadioForm
-                  radio_props={[
-                    {label: 'Male', value: 'male' },
-                    {label: 'Female', value: 'female' }
-                  ]}
-                  initial={'Male'}
-                  onPress={(value) => {this.setState({gender:value})}}
-                />
-              </View>
-              <TouchableHighlight
-                onPress={() => {
-                  this.setModalVisible(!this.state.modalVisible);
-                }}>
-                <View style={{justifyContent: "center", alignItems: "center", marginTop: 40}}>
-                <Button style={{ width : 400, height: 50}} title="Register" onPress={() => {
-                  let stateData = this.state
-                  let user = {
-                    ...stateData.userInfo,
-                    gender: stateData.gender,
-                    birthday: stateData.birthday
-                  }
-                  signUser(user).then(resp => {
-                    console.log('Server Resonse is : ', resp)
-                    this.setState({modalVisible: false})
-                    this.props.navigation.navigate("Landing")
-                  }).catch(msg2 => {
-                    console.log('msg 2 is ; ', msg2)
-                  })
-                }}></Button>
+            <Modal
+              style={{ justifyContent: "center", alignItems: "center" }}
+              animationType="slide"
+              transparent={false}
+              visible={this.state.modalVisible}
+              onRequestClose={() => {
+                Alert.alert("Modal has been closed.");
+              }}
+            >
+              <View style={{ marginTop: 22 }}>
+                <View>
+                  <View
+                    style={{ justifyContent: "center", alignItems: "center" }}
+                  >
+                    <Avatar
+                      rounded
+                      size="xlarge"
+                      title={this.state.userInfo.name}
+                      source={{
+                        uri: this.state.userInfo.photo
+                      }}
+                    />
+                    <Text style={{ marginTop: 10 }}>
+                      {this.state.userInfo.name}
+                    </Text>
+                    <Text style={{ marginTop: 5 }}>
+                      {this.state.userInfo.email}
+                    </Text>
+                  </View>
+                  <View
+                    style={{ justifyContent: "center", alignItems: "center" }}
+                  />
+                  <View
+                    style={{ justifyContent: "center", alignItems: "center" }}
+                  >
+                    <DatePicker
+                      style={{ width: 300, marginTop: 20 }}
+                      date={this.state.birthday}
+                      mode="date"
+                      placeholder="Your Date of Birth"
+                      format="DD-MM-YYYY"
+                      minDate="01-01-1950"
+                      maxDate="01-01-2030"
+                      confirmBtnText="Confirm"
+                      cancelBtnText="Cancel"
+                      customStyles={{
+                        dateIcon: {
+                          position: "absolute",
+                          left: 0,
+                          top: 4,
+                          marginLeft: 0
+                        },
+                        dateInput: {
+                          marginLeft: 36
+                        }
+                      }}
+                      onDateChange={date => {
+                        this.setState({ birthday: date });
+                      }}
+                    />
+                  </View>
+                  <View
+                    style={{
+                      justifyContent: "center",
+                      alignItems: "center",
+                      marginTop: 20
+                    }}
+                  >
+                    <Text>Your Gender</Text>
+                    <RadioForm
+                      radio_props={[
+                        { label: "Male", value: "male" },
+                        { label: "Female", value: "female" }
+                      ]}
+                      initial={"Male"}
+                      onPress={value => {
+                        this.setState({ gender: value });
+                      }}
+                    />
+                  </View>
+                  <TouchableHighlight
+                    onPress={() => {
+                      this.setModalVisible(!this.state.modalVisible);
+                    }}
+                  >
+                    <View
+                      style={{
+                        justifyContent: "center",
+                        alignItems: "center",
+                        marginTop: 40
+                      }}
+                    >
+                      <Button
+                        title="Register"
+                        onPress={() => {
+                          let stateData = this.state;
+                          let user = {
+                            ...stateData.userInfo,
+                            gender: stateData.gender,
+                            birthday: stateData.birthday
+                          };
+                          signUser(user)
+                            .then(resp => {
+                              console.log("Server Resonse is : ", resp);
+                              this.setState({ modalVisible: false });
+                              this.props.navigation.navigate("Landing");
+                            })
+                            .catch(msg2 => {
+                              console.log("msg 2 is ; ", msg2);
+                            });
+                        }}
+                      />
+                    </View>
+                  </TouchableHighlight>
                 </View>
-              </TouchableHighlight>
-            </View>
-          </View>
-        </Modal>
+              </View>
+            </Modal>
           </View>
         </View>
       </View>
@@ -265,4 +314,4 @@ class HomeScreen extends React.Component {
   }
 }
 
-export default HomeScreen
+export default HomeScreen;
