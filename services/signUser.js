82639718709b8b714d33698e77e93eee85901e0a
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-05-24 14:59:56 +0500

    fb login working now

--- services/signUser.js
+++ services/signUser.js
@@ -6,20 +6,15 @@ import firebase from "react-native-firebase";
 const URL = SERVER_URL
 export default signUser =  (userInfo) => {
   return new Promise((resolve, reject) => {
-    console.log("userInfo(1) is : ", userInfo)
     getNotificationToken().then(deviceToken => {
       userInfo.device = deviceToken
-      console.log("device Token is ", userInfo.device)
       if(userInfo.provider === "GOOGLE"){
         if(userInfo){
           console.log("userInfo(Google) is ", userInfo)
           axios.post(`${URL}/api/auth/signup/user`, userInfo).then(res => {
-            // storeUser(userInfo.socialId)
-            console.log("userInfo(Google) res is ", res)
             storeUserData('info', JSON.stringify(res.data.data))
             resolve(res)
           }).catch(error => {
-            console.log('error is ', error)
             reject(error)
           })
     
@@ -29,14 +24,11 @@ export default signUser =  (userInfo) => {
       }
       else{
         if(userInfo){
-          console.log("userInfo(Fb) is ", userInfo)
           axios.post(`${URL}/api/auth/signup/user`, userInfo).then(res => {
-            console.log("userInfo(fb) res is ", res)
             storeUserData('info', JSON.stringify(res.data.data))
             storeUser(userInfo.socialId)
             resolve(res)
           }).catch(error => {
-            console.log('error is ', error)
             reject(error)
           })
         }
@@ -75,7 +67,6 @@ export const removeUserData = async (key, value) => {
 getNotificationToken = async () => {
   const fcmToken = await firebase.messaging().getToken();
   if (fcmToken) {
-    console.log("fcmToken(signUser) is : ", fcmToken);
     return new Promise.resolve(fcmToken)
   } else {
     return new Promise.reject(null)
