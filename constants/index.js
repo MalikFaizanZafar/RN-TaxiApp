c86c3ae8fc37e5ba84aa04c65d887e386220ad65
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-05-28 17:28:49 +0500

    changed sever url

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,9 @@
 // export const SERVER_URL = "http://192.168.1.7:8080"  //office
 
-export const SERVER_URL = "http://139.180.220.85:9080/api-0.0.1"
+// export const SERVER_URL = "http://139.180.220.85:9080/api-0.0.1"
 
 // export const SERVER_URL = "http://192.168.10.5:8080" //home
+
+export const SERVER_URL = "https://dev-backend.subquch.com/api-0.0.1"   //Deployed Backend
+
+
