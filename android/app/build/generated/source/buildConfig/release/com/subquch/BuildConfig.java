08c6f2d5d9d3d9cc6dfecfca0beb388b29477eff
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-07-28 17:15:39 +0500

    changed package name for to taxiapp

--- android/app/build/generated/source/buildConfig/release/com/subquch/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/release/com/subquch/BuildConfig.java
@@ -1,13 +1,13 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.subquch;
+package com.taxiapp;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "com.subquch";
+  public static final String APPLICATION_ID = "com.taxiapp";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 1311;
-  public static final String VERSION_NAME = "1311";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1";
 }
